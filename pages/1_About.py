25fd5b018d7eed15a4ccd9e32a4298dc446102eb
developer1312 <117132370+developer1312@users.noreply.github.com>
2025-09-08 08:10:16 +0530

    Add files via upload

---
+++ pages/1_About.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="About", page_icon="🧮", layout="wide")
+
+st.title("About")
+
+st.write("This is a web application that can solve handwritten math equations.")
